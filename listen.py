1cce936d789e73947bdc85b171245cc60402ffac
J. Michael Burgess <jmichael@hema.to>
2024-06-12 11:40:09 +0200

    fix up to asyc

--- listen.py
+++ listen.py
@@ -1,12 +1,11 @@
+import asyncio
+
 import click
-from click.termui import prompt
 
 from telefilebot import TeleFileBot, read_input_file
-from telefilebot.utils.logging import update_logging_level
-from telefilebot.utils.read_input_file import InputFile
 from telefilebot.directory import Directory
-from telefilebot.utils.logging import setup_logger
-
+from telefilebot.utils.logging import setup_logger, update_logging_level
+from telefilebot.utils.read_input_file import InputFile
 
 log = setup_logger(__name__)
 
@@ -14,23 +13,18 @@ log = setup_logger(__name__)
 @click.command()
 @click.option("--file", help="The input file to start the bot")
 def listen(file: str) -> None:
-
     parameters: InputFile = read_input_file(file)
-
     update_logging_level(parameters.logging.level)
 
     dirs = []
 
-    # gather the directories
-
+    # Gather the directories
     for directory, params in parameters.directories.items():
-
         tmp = Directory(
             path=directory,
             extensions=params.extensions,
             recursion_limit=params.recursion_limit,
         )
-
         dirs.append(tmp)
 
     bot = TeleFileBot(
@@ -41,11 +35,10 @@ def listen(file: str) -> None:
         wait_time=parameters.wait_time,
     )
 
+    async def run_bot():
+        try:
+            await bot.listen()
+        except Exception as e:
+            log.error(f"EXITING due to error: {e}")
 
-    try:
-
-        bot.listen()
-
-    except:
-
-        log.error("EXITING")
+    asyncio.run(run_bot())
--- telefilebot/bot.py
+++ telefilebot/bot.py
@@ -1,20 +1,25 @@
-from typing import List, Dict
-import time
+import asyncio
+import logging
 import math
-import telegram
+from typing import Dict, List
 
-from .utils.logging import setup_logger
-
+from telegram import Bot
+from telegram.ext import Application, ApplicationBuilder
 
 from .directory import Directory
-
+from .utils.logging import setup_logger
 
 logger = setup_logger(__name__)
 
 
 class TeleFileBot:
     def __init__(
-        self, name: str, token: str, chat_id: str, directories: List[Directory], wait_time: int
+        self,
+        name: str,
+        token: str,
+        chat_id: str,
+        directories: List[Directory],
+        wait_time: int,
     ) -> None:
         """
         A generic telegram bot
@@ -24,99 +29,56 @@ class TeleFileBot:
         :param chat_id: the chat id to talk to
         :returns:
         :rtype:
-
         """
 
         logger.debug(f"{name} bot is being constructed")
 
         # create the bot
-
         self._name: str = name
         self._chat_id: str = chat_id
 
-        self._bot: telegram.Bot = telegram.Bot(token=token)
-
+        self._bot: Bot = Bot(token=token)
         self._msg_header = ""
-
         self._directories: List[Directory] = directories
+        self._wait_time: int = int(math.ceil(60 * wait_time))  # in seconds
 
-        self._wait_time: int = int(math.ceil(60 * wait_time)) # in seconds
-
-    def _speak(self, message: str) -> None:
+    async def _speak(self, message: str) -> None:
         """
         send a message
 
         :param message:
         :returns:
         :rtype:
-
         """
 
         full_msg = f"{self._msg_header}{message}"
 
         logger.info(f"{self._name} bot is sending: {message}")
 
-        self._bot.send_message(chat_id=self._chat_id, text=full_msg)
+        await self._bot.send_message(chat_id=self._chat_id, text=full_msg)
 
-    # def _show(self, image: str, description: str):
-    #     """
-    #     send an image
-
-    #     :param image:
-    #     :param description:
-    #     :returns:
-    #     :rtype:
-
-    #     """
-
-    #     full_msg = f"{self._msg_header}{description}"
-
-    #     logger.info(f"{self._name} bot is sending the {description} image")
-
-    #     self._bot.send_photo(
-    #         chat_id=self._chat_id, photo=open(image, "rb"), caption=full_msg
-        # )
-
-    def _check_directories(self) -> None:
-
+    async def _check_directories(self) -> None:
         for directory in self._directories:
-
             new_files: Dict[str, str] = directory.check()
 
             for k, v in new_files.items():
-
                 if v == "new":
-
                     # found a new file
                     msg = f"NEW FILE:\n {k}"
-
-                    self._speak(msg)
-
+                    await self._speak(msg)
                     logger.info(msg)
-
                 elif v == "modified":
-
-                    # foun a modified file
-
+                    # found a modified file
                     msg = f"MODIFIED FILE:\n {k}"
-
-                    self._speak(msg)
-
+                    await self._speak(msg)
                     logger.info(msg)
 
-    def listen(self):
-
-
-        self._speak("Starting up!")
-
+    async def listen(self):
+        await self._speak("Starting up!")
         while True:
-
             try:
-
-                self._check_directories()
-
-                time.sleep(self._wait_time)
-
-            except:
-
-                self._speak("Something went wrong!")
+                await self._check_directories()
+                await asyncio.sleep(self._wait_time)
+            except Exception as e:
+                logger.error(f"Error: {e}")
+                await self._speak("Something went wrong!")
