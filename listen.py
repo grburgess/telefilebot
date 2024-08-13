b59c9da37ad32c4123c7045dea698ab9972830af
J. Michael Burgess <jmichael@hema.to>
2024-08-13 08:45:12 +0200

    test

--- listen.py
+++ listen.py
@@ -1,12 +1,12 @@
+import asyncio
+
 import click
 from click.termui import prompt
 
 from telefilebot import TeleFileBot, read_input_file
-from telefilebot.utils.logging import update_logging_level
-from telefilebot.utils.read_input_file import InputFile
 from telefilebot.directory import Directory
-from telefilebot.utils.logging import setup_logger
-
+from telefilebot.utils.logging import setup_logger, update_logging_level
+from telefilebot.utils.read_input_file import InputFile
 
 log = setup_logger(__name__)
 
@@ -14,25 +14,17 @@ log = setup_logger(__name__)
 @click.command()
 @click.option("--file", help="The input file to start the bot")
 def listen(file: str) -> None:
-
     parameters: InputFile = read_input_file(file)
-
     update_logging_level(parameters.logging.level)
-
     dirs = []
-
     # gather the directories
-
     for directory, params in parameters.directories.items():
-
         tmp = Directory(
             path=directory,
             extensions=params.extensions,
             recursion_limit=params.recursion_limit,
         )
-
         dirs.append(tmp)
-
     bot = TeleFileBot(
         name=parameters.name,
         token=parameters.token,
@@ -40,12 +32,11 @@ def listen(file: str) -> None:
         directories=dirs,
         wait_time=parameters.wait_time,
     )
-
-
     try:
+        asyncio.run(bot.run())
+    except Exception as e:
+        log.error(f"EXITING: {str(e)}")
 
-        bot.listen()
 
-    except:
-
-        log.error("EXITING")
+if __name__ == "__main__":
+    listen()
